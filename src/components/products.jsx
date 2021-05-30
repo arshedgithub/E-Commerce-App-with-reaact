2247c7ee639e316a4895144d533e73a127c21bb7
arshedgithub <arshedahmedmail@gmail.com>
2021-05-30 12:09:21 +0530

    call db

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { getItems } from "./../services/fakeDB";
 
 class Products extends Component {
-  state = {};
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    this.setState({ products: getItems() });
+  }
+
   render() {
-    return <h1>Products</h1>;
+    const items = this.state.products;
+    return (
+      <div className="row">
+        <div className="col-1 text-center">sidebar</div>
+        <div className="col">
+          <p>Showing {items.length} products</p>
+          {items.map((item) => (
+            <div>
+              <div>{item._id}</div>
+              <div>{item.title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
--- src/services/fakeDB.js
+++ src/services/fakeDB.js
@@ -1,57 +1,57 @@
 const items = [
     {
-        _id: 001,
+        _id: "001",
         name: "name",
-        title: laptop,
+        title: "laptop",
         price: 128000,
         numberInStock: 10,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
     {
-        _id: 002,
+        _id: "002",
         name: "name",
-        title: laptop,
+        title: "laptop",
         price: 208000,
         numberInStock: 5,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
     {
-        _id: 003,
+        _id: "003",
         name: "name",
-        title: laptop,
+        title: "laptop",
         price: 120000,
         numberInStock: 11,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
     {
-        _id: 004,
+        _id: "004",
         name: "name",
-        title: tablet,
+        title: "tablet",
         price: 68000,
         numberInStock: 10,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
     {
-        _id: 005,
+        _id: "005",
         name: "name",
-        title: tablet,
+        title: "tablet",
         price: 88000,
         numberInStock: 6,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
     {
-        _id: 006,
+        _id: "006",
         name: "name",
-        title: tablet,
+        title: "tablet",
         price: 78000,
         numberInStock: 8,
         publishedDate: "2018-01-03T19:04:28.809Z"
     },
 
     {
-        _id: 001,
+        _id: "007",
         name: "name",
-        title: phones,
+        title: "phones",
         price: 128000,
         numberInStock: 10,
         publishedDate: "2018-01-03T19:04:28.809Z"
