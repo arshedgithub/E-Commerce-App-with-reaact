89189b64a1b3f696f7201ff61e7ee164fae8b7e6
arshedgithub <arshedahmedmail@gmail.com>
2021-06-01 14:38:14 +0530

    add all icon and content to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,45 +1,78 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faTwitter,
+  faInstagramSquare,
+  faFacebookMessenger,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div class="footer py-3 bg-light">
+    <div class="footer py-2 bg-secondary text-light">
       <div className="footer-content mx-5">
         <div class="container row">
-          <div className="col">
+          <div className="col-5 p-3">
             <h4>Follow us</h4>
-          </div>
-          <div className="col">
-            <h4>Help & Support</h4>
-            <ul className="about-list">
-              <li>
-                <Link to="/contact">Contact us</Link>
+            <ul className="list-inline follow-list">
+              <li className="list-inline-item p-1">
+                <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
               </li>
-              <li>
-                <a href="#">Help center</a>
+              <li className="list-inline-item p-1">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
               </li>
-              <li>
-                <a href="#">Safe payment</a>
+              <li className="list-inline-item p-1">
+                <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
               </li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>About us</h4>
-            <ul className="about-list">
-              <li>
-                <Link to="/about#">About us</Link>
-              </li>
-              <li>
-                <a href="#">Terms & Conditions</a>
-              </li>
-              <li>
-                <a href="#">Privacy & Policy</a>
+              <li className="list-inline-item p-1">
+                <FontAwesomeIcon icon={faFacebookMessenger} size="2x" />
               </li>
             </ul>
+          </div>
+          <div className="col p-3">
+            <h5>Help & Support</h5>
+            <div className="about-list">
+              <NavLink
+                onClick={scrollToTop()}
+                className="nav-link text-light"
+                to="/contact"
+              >
+                Contact us
+              </NavLink>
+              <NavLink className="nav-link text-light" to="#">
+                Help center
+              </NavLink>
+              <NavLink className="nav-link text-light" to="#">
+                Safe payment
+              </NavLink>
+            </div>
+          </div>
+          <div className="col p-3">
+            <h5>About us</h5>
+            <div className="about-list">
+              <NavLink
+                onClick={scrollToTop()}
+                className="nav-link text-light"
+                to="/about"
+              >
+                About us
+              </NavLink>
+              <NavLink className="nav-link text-light" to="#">
+                Terms & Conditions
+              </NavLink>
+              <NavLink className="nav-link text-light" to="#">
+                Privacy & Policy
+              </NavLink>
+            </div>
           </div>
         </div>
-        <hr className="text-muted" />
-        <div class="text-muted text-center">Copyright &copy; Arshed Ahmed</div>
+        <hr />
+        <div class="text-center">Copyright &copy; Arshed Ahmed</div>
       </div>
     </div>
   );
