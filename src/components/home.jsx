8ececa0a6176a6f08be0300365b9e3f9d640886b
arshedgithub <arshedahmedmail@gmail.com>
2021-06-01 17:25:03 +0530

    add home page content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,44 +1,44 @@
-import React, { Component } from "react";
-import sld1 from "../assets/img/slide1.png";
-import sld2 from "../assets/img/slide2.jpg";
-import sld3 from "../assets/img/slide3.jpg";
+import React from "react";
+import bg1 from "../assets/img/home1.png";
+import card1 from "../assets/img/card1.png";
+import card2 from "../assets/img/card2.png";
+import card3 from "../assets/img/card3.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel slide bg-danger"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={sld2} className="d-block w-100" alt="slide1" />
+    <div className="home">
+      <div className="row">
+        <div className="col bg1">
+          <img src={bg1} className="w-100" alt="background" />
         </div>
-        <div className="carousel-item">
-          <img src={sld1} className="d-block w-100" alt="slide2" />
-        </div>
-        <div className="carousel-item">
-          <img src={sld3} className="d-block w-100" alt="slide3" />
+        <div className="col text-center">
+          <h5 className="home mt-5">
+            Larges laptop store in Sri Lanka. <br />
+            Best prices & Leading branded <br />
+            Desktops, Laptops, Tablets, <br /> Mobile phones and Other IT
+            products
+          </h5>
+          <Link to="/products">
+            <button className="btn btn-primary px-4 my-5">Shop Now</button>
+          </Link>
         </div>
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+
+      <div className="row cards">
+        <div className="col card m-2">
+          <img src={card1} alt="card1" className="card-img-top" />
+          <h5 className="p-3">Laptops</h5>
+        </div>
+        <div className="col card m-2">
+          <h5 className="p-3">Mobile Phones</h5>
+          <img src={card2} alt="card2" className="card-img-top" />
+        </div>
+        <div className="col card m-2">
+          <img src={card3} alt="card3" className="card-img-top" />
+          <h5 className="p-3">PC computers</h5>
+        </div>
+      </div>
     </div>
   );
 };
